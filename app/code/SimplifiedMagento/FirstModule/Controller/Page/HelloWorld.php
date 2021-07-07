3d3df48531ee02314d0f0dc02925f231676b1e8a
Ahmed <ahmedchouihi@gmail.com>
2021-07-07 13:22:39 +0100

    'addFunctionToModuleNewController'

---
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace SimplifiedMagento\FirstModule\Controller\Page;
+
+
+use Magento\Framework\App\ResponseInterface;
+
+class HelloWorld extends \Magento\Framework\App\Action\Action
+{
+
+    public function execute()
+    {
+        echo "hello world";
+    }
+}
