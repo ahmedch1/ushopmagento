5dd7b5c09490c4be85aa174dfc0811f4e1b32bce
Ahmed <ahmedchouihi@gmail.com>
2021-07-13 19:21:20 +0100

    eventsAndObserverPart3

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -10,6 +10,7 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\ResponseInterface;
 use SimplifiedMagento\FirstModule\Model\PencilFactory;
 use Magento\Catalog\Model\ProductFactory;
+use Magento\Framework\Event\ManagerInterface;
 
 
 class HelloWorld extends \Magento\Framework\App\Action\Action
@@ -19,26 +20,27 @@ class HelloWorld extends \Magento\Framework\App\Action\Action
     protected $paymentTokenManagement;
     protected $pencilFactory;
     protected $productFactory;
+    protected $_eventManager;
 
-    public function __construct(Context $context, ProductFactory $productFactory, PencilFactory $pencilFactory, PencilInterface $pencilInterface,
+    public function __construct(Context $context,
+                                ManagerInterface $_eventManager, ProductFactory $productFactory, PencilFactory $pencilFactory, PencilInterface $pencilInterface,
                                 PaymentTokenManagementInterface $paymentTokenManagement)
     {
         $this->pencilFactory = $pencilFactory;
         $this->pencilInterface = $pencilInterface;
         $this->paymentTokenManagement = $paymentTokenManagement;
         $this->productFactory = $productFactory;
+        $this->_eventManager = $_eventManager;
         parent::__construct($context);
     }
 
     public function execute()
-   {
+    {
+        $message = new \Magento\Framework\DataObject(array("greeting" => "Good afternoon"));
+        $this->_eventManager->dispatch('custom_event', ['greeting' => $message]);
+        echo $message->getGreeting();
 
-//        $product = $this->productFactory->create()->load(1);
-//        $product->setName("Iphone 6");
-//        $productName = $product->getIdBySku("A00001");
-//        echo $productName;
-//
-        echo "Main function <br/>";
+
     }
 }
 
