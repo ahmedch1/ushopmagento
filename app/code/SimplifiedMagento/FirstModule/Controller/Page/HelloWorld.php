fdfa00c3dce7d89cc24cf92e87bfffc9f638be93
Ahmed <ahmedchouihi@gmail.com>
2021-07-13 13:34:50 +0100

    'beforePluginPart2'

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -9,27 +9,33 @@ use SimplifiedMagento\FirstModule\Api\PencilInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\ResponseInterface;
 use SimplifiedMagento\FirstModule\Model\PencilFactory;
+use Magento\Catalog\Model\ProductFactory;
+
 
 class HelloWorld extends \Magento\Framework\App\Action\Action
+
 {
     protected $pencilInterface;
     protected $paymentTokenManagement;
     protected $pencilFactory;
+    protected $productFactory;
 
-    public function __construct(Context $context, PencilFactory $pencilFactory, PencilInterface $pencilInterface,
+    public function __construct(Context $context, ProductFactory $productFactory, PencilFactory $pencilFactory, PencilInterface $pencilInterface,
                                 PaymentTokenManagementInterface $paymentTokenManagement)
     {
         $this->pencilFactory = $pencilFactory;
         $this->pencilInterface = $pencilInterface;
         $this->paymentTokenManagement = $paymentTokenManagement;
+        $this->productFactory=$productFactory;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        $pencil = $this->pencilFactory->create(array("name" => "Bob", "school" => "International college"));
-        echo '<pre>';
-        var_dump($pencil);
+        $product=$this->productFactory->create()->load(1);
+        $product->setName("Iphone 6");
+        $productName=$product->getName();
+        echo $productName;
     }
 }
 
