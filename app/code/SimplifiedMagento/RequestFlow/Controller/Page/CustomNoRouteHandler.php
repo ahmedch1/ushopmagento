06323c11eb2d13e04f9c937655bd5005f900d867
Ahmed Ch <ahmedchouihi@gmail.com>
2021-08-27 19:56:00 +0100

    'customRouterTobeReviewed'

--- app/code/SimplifiedMagento/RequestFlow/Controller/Page/CustomNoRouteHandler.php
+++ app/code/SimplifiedMagento/RequestFlow/Controller/Page/CustomNoRouteHandler.php
@@ -1,10 +1,16 @@
 <?php
-
-
 namespace SimplifiedMagento\RequestFlow\Controller\Page;
 
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Router\NoRouteHandlerInterface;
 
-class CustomNoRouteHandler
+class CustomNoRouteHandler implements NoRouteHandlerInterface
 {
 
+    public function process(RequestInterface $request)
+    {
+        $request->setRouteName('noroutefound')->setControllerName('page')->setActionName('customnoroute');
+
+        return true;
+    }
 }
